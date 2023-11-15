a58f5fd189eed11e96957208149942c82fd58112
mattymassey95 <114009907+elfedmaths@users.noreply.github.com>
2023-11-15 20:59:43 +0000

    Update game.script.js

--- circle/js/game.script.js
+++ circle/js/game.script.js
@@ -5,8 +5,8 @@ let color = '#ACD3ED';
 
 document.addEventListener('mousedown', start);
 document.addEventListener('mouseup', stop);
-document.addEventListener('touchstart', start);
-document.addEventListener('touchend', stop);
+document.body.addEventListener('touchstart', start);
+document.body.addEventListener('touchend', stop);
 window.addEventListener('resize', resize);
 
 function resize() {
